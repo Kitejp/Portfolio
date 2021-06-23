107b172b5e272aed365c1e8ec170562e05e9652c
unknown <74812393+Kitejp@users.noreply.github.com>
2021-06-23 20:30:48 +0900

    ポートフォリオ
    私が個人で制作した２Dアクションゲームです。

---
+++ SourceCodes/Fire.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float fireSpeed = 10f;
+    public float spawnTime = 0.1f;
+    private float currentTime = 0f;
+
+    private Rigidbody2D rb;
+
+    private GameObject player;
+    private GameObject boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+        boss = GameObject.Find("Boss");
+
+        if (boss != null)
+        {
+            if (player.transform.position.x > boss.transform.position.x)
+            {
+                fireSpeed = -fireSpeed;
+                transform.localScale = new Vector3(5, 5, 5);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        FireMove();
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime > spawnTime)
+            Destroy(this.gameObject);
+    }
+
+    private void FireMove()
+    {
+        Vector2 fireMovement = new Vector2(-1,0).normalized;
+
+        rb.velocity = fireMovement * fireSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Destroy(this.gameObject);
+    }
+}
---
+++ SourceCodes/TitleManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TitleManager : MonoBehaviour
+{
+    [Header("フェード")]public FadeImageManager fade;
+
+    public TextMeshProUGUI exText, backText, titleText, startText, goText, quitText;
+
+    private bool firstPush = false;
+    private bool goNextScene = false;
+
+    private void Start()
+    {
+        exText.transform.parent.gameObject.SetActive(false);
+        backText.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void PressStart()
+    {
+        if (!firstPush)
+        {
+            fade.StartFadeOut();
+            firstPush = true;
+        }
+    }
+
+    public void Explanation()
+    {
+        exText.transform.parent.gameObject.SetActive(true);
+        backText.transform.parent.gameObject.SetActive(true);
+        startText.transform.parent.gameObject.SetActive(false);
+        goText.transform.parent.gameObject.SetActive(false);
+        quitText.transform.parent.gameObject.SetActive(false);
+        titleText.gameObject.SetActive(false);
+    }
+
+    public void Back()
+    {
+        exText.transform.parent.gameObject.SetActive(false);
+        backText.transform.parent.gameObject.SetActive(false);
+        startText.transform.parent.gameObject.SetActive(true);
+        goText.transform.parent.gameObject.SetActive(true);
+        quitText.transform.parent.gameObject.SetActive(true);
+        titleText.gameObject.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(!goNextScene && fade.IsFadeOutComplete())
+        {
+            GameManager.instance.stageNum = 1;
+            SceneManager.LoadScene("stage" + GameManager.instance.stageNum);
+            goNextScene = true;
+        }
+    }
+}
